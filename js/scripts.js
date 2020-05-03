76a185cd7ff7083846f92154df3cd3ab3c852804
Lucie <lucie.tessier.lms@gmail.com>
2020-05-03 16:18:27 +0200

    portfolio front end OK correction orthographique OK

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+(function ($) {
+    "use strict"; // Mode strict
+
+    // Paramétrage du défilement
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // En mode responsive, permet de fermer le menu déroulant lors d'un click sur un item de la nav
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Scrollpsy (nav page active)
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    navbarCollapse();
+    $(window).scroll(navbarCollapse);
+})(jQuery);
